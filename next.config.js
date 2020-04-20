976083c45ecfc92460723b08cc4af269c43a4030
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-20 16:15:04 -0300

    reset to plain next starter

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  },
-};
--- pages/github/authorizing.tsx
+++
@@ -1,6 +0,0 @@
-import { useGithubAuthRedirect } from 'react-tinacms-github';
-
-export default function Authorizing() {
-  useGithubAuthRedirect();
-  return <h2>Authorizing with Github, Please wait...</h2>;
-}
